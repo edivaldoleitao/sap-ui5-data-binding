fef281d155cea2bb027a213166dbfc75aa80038d
EDIVALDO LEITAO DE NETO <mandoritos.ea@gmail.com>
2024-02-19 14:07:32 +0000

    criando JSONModel para o databinding

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -9,7 +9,52 @@ sap.ui.define([
 
         return Controller.extend("sap.databinding2.project3.controller.View1", {
             onInit: function () {
+                var obj = {
+                    name: "Ronaldo",
+                    last_name: "Santos",
+                    age: 35,
+                    address: {
+                    edit: false,
+                    street: {
+                    name: "Rua Teste",
+                    number: 13,
+                    },
+                    city: "Itatiba",
+                    state: "SP"
+                    },
+                    skills: [{name:"ui5"}, {name:"hana"},{name:"abap"},{name:"java"},{name:"cpi"}],
+                    fan_of: [
+                    {
+                    name: "Pele",
+                    job: "Former soccer player"
+                    },
+                    {
+                    name: "Michael Jordan",
+                    job: "Basketball player"
+                    },
+                    {
+                    name: "Renato Russo",
+                    job: "Singer"
+                    },
+                    {
+                    name: "Axl Rose",
+                    job: "Singer"
+                    }
+                    ],
+                    social: [
+                    {
+                    name: "Twitter",
+                    value: "http://twitter.com/"
+                    },
+                    {
+                    name: "Linkedin",
+                    value: "http://www.linkedin.com/"
+                    }
+                    ]
+                    };
 
+                    var model = new sap.ui.model.json.JSONModel(obj);
+                    this.getView().setModel(model,"perfil");
             }
         });
     });
